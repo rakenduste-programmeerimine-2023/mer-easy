9521b5ee5e546066b3f7ab1d1f7ba9cc62376960
stevenmaasi <zteven@tlu.ee>
2023-11-18 17:55:01 +0200

    #9 - Redirect to home page after successful sign in.

---
+++ app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+            Sisselogitud!
+        </div>
+    )
+}
