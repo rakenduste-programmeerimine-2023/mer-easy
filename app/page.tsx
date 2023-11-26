efd587269893c23db30adf58b78db76249a9d888
Daniel Kupinski <kupinskidaniel+github@gmail.com>
2023-11-26 13:44:54 +0200

    #10
    #10 - sign in ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import '@/app/styles/loginStyles.scss'
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
 import {redirect} from "next/navigation";
+
+const logoPath = 'app/MerEasyLogo.png';
 
 export default function Login({searchParams}: {
   searchParams: { message: string }
@@ -24,34 +27,38 @@ export default function Login({searchParams}: {
       return redirect('/home')
   }
 
-  return (
-      <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-        <form
-            className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-            action={signIn}
-        >
-          <label className="text-md" htmlFor="email">Email</label>
-          <input
-              className="rounded-md px-4 py-2 bg-inherit border mb-6"
-              name="email"
-              placeholder="you@example.com"
-              required
-          />
-          <label className="text-md" htmlFor="password">Password</label>
-          <input
-              className="rounded-md px-4 py-2 bg-inherit border mb-6"
-              type="password"
-              name="password"
-              placeholder="••••••••"
-              required
-          />
-          <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">Sign In</button>
-          {searchParams?.message && (
-              <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-                {searchParams.message}
-              </p>
-          )}
-        </form>
-      </div>
-  )
+    return (
+        <div className="flex-1 login-container">
+            <form className="animate-in login-form" action={signIn}>
+                <img src={logoPath} alt="Company Logo" className="logo" />
+                <div className="text-to-bottom">
+                <label className="text-md" htmlFor="email">
+                    Email
+                </label>
+                <input
+                    className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                    name="email"
+                    placeholder="you@example.com"
+                    required
+                />
+                <label className="text-md" htmlFor="password">
+                    Password
+                </label>
+                <input
+                    className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                    type="password"
+                    name="password"
+                    placeholder="••••••••"
+                    required
+                />
+                <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">Sign In</button>
+                {searchParams?.message && (
+                    <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+                        {searchParams.message}
+                    </p>
+                )}
+                </div>
+            </form>
+        </div>
+    );
 }
