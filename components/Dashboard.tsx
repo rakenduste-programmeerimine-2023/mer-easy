4190c05731b1723d1e931578a008a7096608652f
Daniel Kupinski <kupinskidaniel+github@gmail.com>
2024-01-14 17:52:16 +0200

    #57 update functionality added

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,6 +1,7 @@
-import { Routes } from "@/app/routes/routes";
-import { useRouter } from "next/navigation";
+import {Routes} from "@/app/routes/routes";
+import {useRouter} from "next/navigation";
 import '@/app/styles/menu.scss';
+import {useEffect} from "react";
 
 export default function Dashboard() {
     const router = useRouter();
@@ -9,20 +10,30 @@ export default function Dashboard() {
         await router.push(route);
     };
 
+    useEffect(() => {
+        const handlePopState = () => {
+            window.location.reload();
+        };
+
+        window.addEventListener('popstate', handlePopState);
+
+        return () => {
+            window.removeEventListener('popstate', handlePopState);
+        };
+    }, []);
+
     return (
         <div className="dashboard-content">
             <div className="picture-container">
-                {/* First Picture and Button */}
                 <img src="meritaktiva.png" alt="First Image" className="medium-sized-image" />
-                <button className="nav-button" onClick={() => changePage(Routes.MERIT_ITEMS)}>
+                <button className="nav-button" onClick={() => {changePage(Routes.MERIT_ITEMS);}}>
                     Update Merit and view table
                 </button>
             </div>
 
             <div className="picture-container">
-                {/* Second Picture and Button */}
                 <img src="MRPeasy.png" alt="Second Image" className="medium-sized-image" />
-                <button className="nav-button" onClick={() => console.log('MRP pressed')}>
+                <button className="nav-button" onClick={() => {changePage(Routes.MRPEASY_ITEMS);}}>
                     Update MRPeasy and view table
                 </button>
             </div>
